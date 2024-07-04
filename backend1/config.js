c5993e909defcfdaeb0d523cfc98876765d81e70
nishant kashyap <kashyapnishant833@gmail.com>
2024-07-04 19:02:03 +0530

    first commit

---
+++ backend1/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    jwtSecret: 'your_jwt_secret',
+    mongoURI: 'mongodb+srv://kashyapnishant833:wmO215XCPxgSxmOZ@cuvettefinalprojectdata.phcg5tk.mongodb.net/?retryWrites=true&w=majority&appName=CuvettefinalprojectDATABASE',
+  };
+  
